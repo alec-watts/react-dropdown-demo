0126d7ae4f1e5bbc0145921d123b06413723c5b0
alec-watts <alecwatts1@gmail.com>
2023-03-14 14:11:48 -0400

    up to date

--- src/App.js
+++ src/App.js
@@ -17,10 +17,34 @@ const App = () => {
       <NavItem icon={ <MessengerIcon /> } />
 
       <NavItem icon={ <CaretIcon /> }>
-
+        <DropdownMenu />
       </NavItem>
     </NavBar>
 
+  )
+}
+
+const DropdownMenu = () => {
+
+  const DropdownItem = (props) => {
+    return (
+      <a href='#' className='menu-item'>
+        <span className='icon-button'> { props.leftIcon } </span>
+
+        { props.children }
+
+        <span className='icon-right'> { props.rightIcon } </span>
+      </a>
+    )
+  }
+
+  return (
+    <div className='dropdown'>
+      <DropdownItem> My Profile </DropdownItem>
+      <DropdownItem leftIcon={ <CogIcon /> } rightIcon={ <ChevronIcon /> }>
+        Settings
+      </DropdownItem>
+    </div>
   )
 }
 
@@ -34,14 +58,17 @@ const NavBar = (props) => {
 }
 
 const NavItem = (props) => {
-  
+
   const [open, setOpen] = useState(false)
 
   return (
     <li className='nav-item'>
-      <a href='#' className='icon-button'>
+      <a href='#' className='icon-button' onClick={() => setOpen(!open)}>
         { props.icon }
       </a>
+
+      {/* "open &&" checks if open is true and displays the following data if true" */}
+      { open && props.children }
     </li>
   )
 }
